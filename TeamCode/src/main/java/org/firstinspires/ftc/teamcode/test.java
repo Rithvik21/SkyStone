7efaaaf7386e265e08b71978e5eca77ab6596add
Rithvik21 <58535240+Rithvik21@users.noreply.github.com>
2019-12-04 18:45:56 -0600

    test.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test.java
@@ -13,8 +13,17 @@ rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
 
 waitForStart();
 
-leftDrive.setPower(power);
-rightDrive.setPower(-power);
-sleep(1000);
+drive(motorPower(.4),1000);
+drive(motorPower(.2),300);
+drive(motorPower(-.3),100);
+} 
+ public double motorPower (double speed) {
+   double power = 1*speed;
+   return power;
+ }
+  public void drive(double power, double time) {
+    leftDrive.setPower(power);
+      rightDrive.setPower(-power);
+    sleep(time);
 }
-}
+
