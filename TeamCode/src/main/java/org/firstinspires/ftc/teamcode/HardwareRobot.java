6b54ca7d8e6e43a9a39669924aba47e548d7e06b
Rithvik21 <58535240+Rithvik21@users.noreply.github.com>
2019-12-04 18:56:38 -0600

    HardwareRobot.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareRobot.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode;
+
+public class HardwareRobot {
+public DcMotor leftDrive, rightDrive;
+
+public HardwareRobot()  {}
+public void init(hardwareMap hwMap) {
+hardwareMap = hwMap;
+leftDrive = hardwareMap.get(DcMotor.class, "left_drive");
+rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
+
+leftDrive.setDirection(DcMotor.Direction.FORWARD);
+rightDrive.setDirection(DcMotor.Direction.BACKWARD);
+}
+}
